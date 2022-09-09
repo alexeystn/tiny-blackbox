c195b58e2789e44756bbc031b1155cb982fc2c6e
Alexey Stankevich <alexey.stn@gmail.com>
2022-09-09 23:38:31 +0300

    Use circular UART DMA mode

--- Firmware/Core/Src/stm32g0xx_hal_msp.c
+++ Firmware/Core/Src/stm32g0xx_hal_msp.c
@@ -290,7 +290,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     hdma_usart1_rx.Init.MemInc = DMA_MINC_ENABLE;
     hdma_usart1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
     hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_usart1_rx.Init.Mode = DMA_NORMAL;
+    hdma_usart1_rx.Init.Mode = DMA_CIRCULAR;
     hdma_usart1_rx.Init.Priority = DMA_PRIORITY_LOW;
     if (HAL_DMA_Init(&hdma_usart1_rx) != HAL_OK)
     {
