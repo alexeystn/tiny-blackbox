4bbc962503fb855bcd1a24e127eaab8679d524fa
Alexey Stankevich <alexey.stn@gmail.com>
2022-09-11 01:56:51 +0300

    Add scripts for full memory test

---
+++ Python/utils/test_sequence_transmit.py
@@ -0,0 +1,46 @@
+# Send continuous ASCI sequence of incrementing numbers to Serial port
+# Each number is encoded with 8 characters:
+# '00000000', '00000001', '00000002', ..... '02097152'
+
+import time
+import serial
+
+port_name = '/dev/cu.usbserial-A50285BI'
+
+full_memory_size = 128 * 1024 * 1024 // 8  # 128 MBit flash chip
+
+bytes_per_number = 8
+packet_length = 50
+packet_interval_sec = 0.003
+
+
+def encode_number(n):
+    return '{0:08d}'.format(n).encode()
+
+
+class SpeedMeasure:
+    previous_timestamp = 0
+    counter = 0
+
+    def put(self, n):
+        self.counter += n
+        current_timestamp = time.time()
+        if current_timestamp - self.previous_timestamp > 1:  # print 1 time / second
+            t_diff = current_timestamp - self.previous_timestamp
+            speed = self.counter / t_diff
+            self.counter = 0
+            self.previous_timestamp = current_timestamp
+            print('{0:.0f} kbit/s'.format(speed / 1000 * 8), end='\t')
+            return 1
+        return 0
+
+
+sm = SpeedMeasure()
+
+with serial.Serial(port_name, baudrate=1500000) as ser:
+    for i in range(full_memory_size // bytes_per_number):
+        ser.write(encode_number(i))
+        if (i + 1) % packet_length == 0:
+            time.sleep(packet_interval_sec)
+            if sm.put(bytes_per_number * packet_length):
+                print('{0:.1f}%'.format(i / (full_memory_size // bytes_per_number) * 100))
---
+++ Python/utils/test_sequence_verify.py
@@ -0,0 +1,21 @@
+# Check if log created with 'test_sequence_transmit'
+# was successfully recorded and read from flash.
+
+filename = '../xxx.bbl'
+
+with open(filename, 'rb') as f:
+    data = f.read()
+
+bytes_per_count = 8
+size = len(data)//bytes_per_count
+error_count = 0
+
+for i in range(size):
+    fragment = data[i*bytes_per_count:(i+1)*bytes_per_count]
+    original = '{0:08d}'.format(i).encode()
+    if fragment != original:
+        print(original, '!=', fragment)
+        error_count += 1
+        if error_count > 20:
+            print('Too many errors')
+            break
