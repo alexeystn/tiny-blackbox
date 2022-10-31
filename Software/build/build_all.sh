5e55f62568a3dbcaedf566a758df7cdabdc61409
Alexey Stankevich <alexey.stn@gmail.com>
2022-10-31 22:27:25 +0300

    Fix comments

--- Software/build/build_all.sh
+++ Software/build/build_all.sh
@@ -4,26 +4,26 @@ VER="1.0.0"
 
 cd ../GUI/
 
-# build executable for macOS
+# Build executable for macOS
 pyinstaller tinybb_gui.py --name="Tiny_Blackbox" --windowed --onefile --icon "tinybb.icns"
 
-# remove spec-file generated in previous step
-# it must not be used with Windows PyInstaller
+# Remove spec-file generated in previous step, 
+# because it must not be used with Windows PyInstaller
 rm Tiny_Blackbox.spec
 
-# run Windows PyInstaller in Docker
-# this use "tinybb_gui_win.spec" file with removed "hooksconfig={}" line
-# option A: run public image from repository
+# Run Windows PyInstaller in Docker
+# This use "tinybb_gui_win.spec" file with removed "hooksconfig={}" line
+# Option A: run public image from repository
 ## docker run --rm -v "$PWD":/src/ cdrx/pyinstaller-windows:python3
-# option B: run personal container with pre-installed dependencies
-docker run --rm -v "$PWD":/src/ pyinstaller-windows-add
+# Option B: run personal container with pre-installed dependencies
+docker run --rm -v "$PWD":/src/ pyinstaller-windows
 
-# move executables to "output"
+# Move executables to "output"
 mkdir ../build/output
 mv dist/windows/Tiny_Blackbox.exe "../build/output/Tiny_Blackbox_"$VER".exe"
 mv dist/Tiny_Blackbox.app "../build/output/Tiny_Blackbox_"$VER".app"
 
-# remove temporary files
+# Remove temporary files
 rm -r dist
 rm -r __pycache__
 rm -r build
